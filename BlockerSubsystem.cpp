957712d1e06ca73c7e3ebd46e0d0c2d6b230453f
Programmers <prog-hp1@core2062.com>
2013-04-20 10:06:44 -0500

    Tested and changed blocker speed
    got limit switches working

--- BlockerSubsystem.cpp
+++ BlockerSubsystem.cpp
@@ -15,7 +15,9 @@ std::string BlockerSubsystem::name(void){
 }
 
 void BlockerSubsystem::robotInit(void){
-	SmartDashboard::PutNumber("blocker-mult", .15);
+	SmartDashboard::PutNumber("blocker-mult", .9);
+	SmartDashboard::PutBoolean("top", false);
+	SmartDashboard::PutBoolean("bottom", false);
 }
 
 void BlockerSubsystem::teleopInit(void){
@@ -45,12 +47,15 @@ void BlockerSubsystem::teleopLogic(void){
 }
 
 void BlockerSubsystem::teleopOutput(void){
+	SmartDashboard::PutBoolean("top", top.Get());
+	SmartDashboard::PutBoolean("bottom", bottom.Get());
+
 	blockOut *= SmartDashboard::GetNumber("blocker-mult");
 	cout << bottom.Get() << top.Get() <<endl;
-//	if (bottom.Get() and blockOut < 0){
-//		blockOut = 0;
-//	} else if (top.Get() and blockOut > 0){
-//		blockOut = 0;
-//	}
-	winch.Set(-blockOut);
+	if (bottom.Get() and blockOut < 0){
+		blockOut = 0;
+	} else if (top.Get() and blockOut > 0){
+		blockOut = 0;
+	}
+	winch.Set(blockOut);
 }
