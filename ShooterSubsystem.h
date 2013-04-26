55cd65bd58da94dd891f99f42419ca59702ef57f
Programmers <prog-hp1@core2062.com>
2013-04-26 10:56:54 -0500

    Increased shooter speed

--- ShooterSubsystem.h
+++ ShooterSubsystem.h
@@ -40,7 +40,7 @@ class ShooterSubsystem : public CORESubsystem{
 	SensorEdge		pusherSensor;
 	
 	static const float shooterInc = 1;		 // value is in rps
-	static const float shooterDefault = 62;
+	static const float shooterDefault = 64;
 	
 	float shooterValue;
 	float shooterOutput;
